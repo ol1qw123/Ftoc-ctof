6d33dd46b6dfc50a810246712454c1bc2479d778
FU <Fmothoughts@gmail.com>
2024-07-19 20:59:55 +0300

    first commit

---
+++ function.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <iostream>
+
+class function {
+public:
+    double cTof(double c) {
+        double result = c * 9 / 5 + 32;
+        return result;
+    }
+
+    double fToc(double f) {
+        double result = (f - 32) * 5 / 9;
+        return result;
+    }
+
+    void opt() {
+        double c;
+        double f;
+        int intWhichFunction;
+        bool WhichFunction = false;
+        double result;
+        char typex;
+
+        std::cout << "Choose What Function you want by number (1 = C to F / 2 = F to C) : ";
+        std::cin >> intWhichFunction;
+        if (intWhichFunction == 1) {
+            WhichFunction = true;
+        }
+
+        if (WhichFunction) {
+            std::cout << "What is C : ";
+            std::cin >> c;
+            result = cTof(c);
+            typex = 'F';
+        }
+        else {
+            std::cout << "What is F : ";
+            std::cin >> f;
+            result = fToc(f);
+            typex = 'C';
+        }
+
+        std::cout << "The Result is " << result << " " << typex << "\n";
+    }
+
+    void end() {
+        std::cout << "Hope you liked the program\n";
+    }
+
+    void work() {
+        int againint;
+        bool againbool = true;
+
+        while (againbool) {
+            opt();
+
+            std::cout << "Do you want to do it again? (1 = Yes / 2 = No): ";
+            std::cin >> againint;
+
+            if (againint == 1) {
+                againbool = true;
+            }
+            else if (againint == 2) {
+                againbool = false;
+            }
+            else {
+                std::cout << "Invalid input. Ending program.\n";
+                againbool = false;
+            }
+        }
+
+        end();
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "function.hpp"
+function fun;
+
+
+
+int main() {
+	
+
+	fun.work();
+	
+}
